f1852c5549e14c9e3592f23824711d8c4b6f8c6c
Nazmul Alom <nazmulshuvo03@gmail.com>
2025-11-25 16:02:21 +0600

    api url fixed

--- api/contact-server.js
+++ api/contact-server.js
@@ -5,7 +5,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5050;
 
 // Middleware
 app.use(cors());
